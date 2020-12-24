a1f7102b9bf20e8d45155f278cc2fb3c4fceb39e
abhishek <abhishek@linux>
2020-12-24 22:05:47 +0530

    first commit

---
+++ src/test/java/com/lizard/edu/eip/cbr/CamelContentBasedRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizard.edu.eip.cbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelContentBasedRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
